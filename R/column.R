5b24d0708427ecb17009942c35d2998d8be18964
Lindsay Jorgenson <ljorgenson@astho.org>
2024-06-05 11:48:24 -0500

    add: stacked bar

--- R/column.R
+++ R/column.R
@@ -105,6 +105,9 @@ create_single_bar <- function(
         format = y_format
       )
     ) %>%
+    highcharter::hc_colors(
+      colors = my_colors
+    ) %>%
     highcharter::hc_title(
       text = title_text, # title text
       margin = 40,
@@ -171,4 +174,197 @@ create_single_bar <- function(
     )
   return(hc)
 }
+
+
+#' NACCHO Stacked Bar Chart
+#'
+#' @description
+#' Create an accessible stacked bar chart (vertical or horizontal) using a NACCHO theme and the {highcharter} package.
+#'
+#' @param data the data frame to create the map.
+#' @param type character string; must be "column" or "bar", where "column" displays vertically and "bar" displays horizontally.
+#' @param x_variable object; from the data frame, ideally a character.
+#' @param y_variable numeric; from the data frame.
+#' @param x_group character object; from the data frame.
+#' @param tick_amount numeric; sets the number of ticks, defaults to 6.
+#' @param color_bars boolean, TRUE or FALSE, where TRUE results in each bar being a different color.
+#' @param y_label character string; label for the y-axis.
+#' @param y_max numeric; the maximun value of the y-axis, defaults to 100.
+#' @param y_format character string; format for the y-axis numbers, defaults to "{value}". This is used to adjust decimals or add characters after the number, e.g., "{value}%".
+#' @param my_colors vector or character string; one character or a vector of characters, which must be defined hex values. Defaults to main_colors.
+#' @param title_text character string; title of the visualization. Defaults to nothing.
+#' @param subtitle_text character string; subtitle to appear underneath the title. Defaults to nothing.
+#' @param caption_text character string; source and data notes to appear underneath the figure. Defaults to nothing.
+#' @param caption_align character string; options include "left", "center", or "right" where the default is "center".
+#' @param legend_enable boolean, TRUE or FALSE, where TRUE enables the legend. Defaults to FALSE.
+#' @param legend_title character string; title of the legend, defaults to nothing unless defined.
+#' @param tooltip character string; text and values to appear in the tooltip, use {point.xx} to call a value from the data frame, where xx is a specific column.
+#' @param select_theme hc_theme, must be a defined hc_theme. Options from within this package include naccho_minimal and naccho_normal.
+#' @param allow_export boolean, TRUE or FALSE, where true allows the visualization to be exported.
+#' @param accessible_desc character string; alternative text description of the figure for screen readers.
+
+
+#'
+#' @return A column or bar chart from highcharter.
+#' @export
+#'
+#' @examples
+#' test_data <- data.frame(
+#'   year = c("2019", "2022"),
+#'   value = c(150, 200, 20, 30),
+#'   bender = c("Fire Bender", "Fire Bender", "Water Bender", "Water Bender")
+#' )
+
+#' test_data <-
+#' within(test_data, bender <- factor(bender, levels=c("Water Bender", "Fire Bender")))
+
+#' test_data <- test_data %>%
+#'   dplyr::arrange(bender)
+
+
+#' create_stacked_bar(
+#'   data = test_data,
+#'   my_colors = c("#8DA54D", "#3C8A97"),
+#'   x_variable = "year",
+#'   y_variable = "value",
+#'   x_group = "bender",
+#'   y_label = "Number",
+#'   y_max = 250,
+#'   legend_enable = TRUE,
+#'   legend_title = "Bender Type",
+#'   tooltip =
+#'     "Bender: {point.bender} <br>
+#'  Value: {point.value}"
+#' )
+
+create_stacked_bar <- function(
+    data,
+    type = "column",
+    x_variable,
+    y_variable,
+    x_group,
+    tick_amount = 6,
+    color_bars = FALSE,
+    y_label,
+    y_max = 100,
+    y_format = "{value}",
+    my_colors = main_colors,
+    title_text = "",
+    subtitle_text = "",
+    caption_text = "",
+    caption_align = "center",
+    legend_enable = FALSE,
+    legend_title = "",
+    tooltip,
+    select_theme = naccho_minimal,
+    allow_export = TRUE,
+    accessible_desc = ""
+) {
+
+  hc <-
+    highcharter::hchart(
+      data,
+      type = type,
+      highcharter::hcaes(
+        x = .data[[x_variable]],
+        y = .data[[y_variable]],
+        group = .data[[x_group]]
+      ),
+      stacking = "normal"
+    ) %>%
+    highcharter::hc_add_dependency(
+      name = "modules/accessibility.js"
+    ) %>%
+    highcharter::hc_plotOptions(
+      accessibility = list(
+        enabled = TRUE,
+        description = accessible_desc,
+        keyboardNavigation = list(
+          enabled = TRUE
+        )
+      )
+    ) %>%
+    highcharter::hc_xAxis(
+      title = list(text = "") # x-axis label
+    ) %>%
+    highcharter::hc_yAxis(
+      title = list(text = y_label), # y-axis label
+      max = y_max,
+      tickAmount = tick_amount,
+      min = 0,
+      labels = list(
+        format = y_format
+      )
+    ) %>%
+    highcharter::hc_colors(
+      colors = my_colors
+    ) %>%
+    highcharter::hc_title(
+      text = title_text, # title text
+      margin = 40,
+      widthAdjust = -60
+    ) %>%
+    highcharter::hc_subtitle(
+      text = subtitle_text # text under title
+    ) %>%
+    highcharter::hc_caption(
+      text = caption_text, # source and data notes
+      useHTML = TRUE,
+      margin = 20,
+      align = caption_align
+    ) %>%
+    highcharter::hc_legend(
+      enabled = legend_enable, # enabled legend
+      reversed = TRUE,
+      layout = "horizontal",
+      align = "center",
+      verticalAlign = "bottom",
+      itemMarginBottom = 3,
+      itemMarginTop = 3,
+      margin = 22,
+      title = list(
+        text = legend_title
+      )
+    ) %>%
+    highcharter::hc_tooltip(
+      pointFormat = tooltip,
+      useHTML = TRUE
+    ) %>%
+    highcharter::hc_add_theme(
+      select_theme
+    ) %>%
+    highcharter::hc_exporting(
+      enabled = allow_export,
+      accessibility = list(
+        enabled = TRUE
+      ),
+      formAttributes = list(
+        target = "_blank" # opens in new window
+      ),
+      sourceHeight = 700, # sets height of download
+      sourceWidth = 1200, # sets width of download
+      allowHTML = TRUE,
+      url = "https://exporter.opifex.org", #serve owned by John Coene
+      buttons = list(
+        contextButton = list(
+          symbol = "menu",
+          symbolStrokeWidth = 2,
+          symbolFill = teal,
+          symbolStroke = teal,
+          menuItems = c("printChart",
+                        "separator",
+                        "downloadPNG",
+                        "downloadJPEG",
+                        "downloadPDF",
+                        "downloadSVG",
+                        "separator",
+                        "downloadCSV",
+                        "downloadXLS",
+                        "openInCloud")
+        )
+      )
+    )
+  return(hc)
+}
+
 
