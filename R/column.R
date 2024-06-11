e9d042abf15ac00cba33ecec0b24ee4ab9fbdfaf
Lindsay Jorgenson <ljorgenson@astho.org>
2024-06-11 17:35:17 -0500

    add: new tick theme and documentation

--- R/column.R
+++ R/column.R
@@ -577,3 +577,170 @@ create_grouped_bar <- function(
   return(hc)
 }
 
+
+
+
+#' NACCHO Tick Bar Chart
+#'
+#' @description
+#' Create an accessible ticked bar chart (horizontal) using a NACCHO theme and the {highcharter} package.
+#'
+#' @param data the data frame to create the map.
+#' @param type character string; must be "column" or "bar", where "column" displays vertically and "bar" displays horizontally.
+#' @param x_variable object; from the data frame, ideally a character.
+#' @param y_variable numeric; from the data frame.
+#' @param x_group character object; from the data frame.
+#' @param size numeric; controls width of figure, defaults to 400.
+#' @param y_format character string; format for the y-axis numbers, defaults to "{value:,.0f}". This is used to adjust decimals or add characters after the number. Examples follow. "{value}%" add a % sign and does not specify decimals. "{value:.1f}" specifies one decimal point. "{value:,.0f}" specifies one decimal and adds a comma every three values. "{value}%" adds a percent sign. You can also add words to the value by doing the following: "{value} words here".
+#' @param my_colors vector or character string; one character or a vector of characters, which must be defined hex values. Defaults to naccho.viz::naccho_colors.
+#' @param title_text character string; title of the visualization. Defaults to nothing.
+#' @param subtitle_text character string; subtitle to appear underneath the title. Defaults to nothing.
+#' @param caption_text character string; source and data notes to appear underneath the figure. Defaults to nothing.
+#' @param caption_align character string; options include "left", "center", or "right" where the default is "center".
+#' @param tooltip character string; text and values to appear in the tooltip, use {point.xx} to call a value from the data frame, where xx is a specific column.
+#' @param allow_export boolean, TRUE or FALSE, where true allows the visualization to be exported.
+#' @param accessible_desc character string; alternative text description of the figure for screen readers.
+#'
+#' @return A ticked chart from highcharter.
+#' @export
+#'
+#' @examples
+#' test_data <- base::data.frame(
+#'   action = c("Expanded", "Reduced", "No Change", "Expanded", "Reduced", "No Change"),
+#'   value = c(6.5, 1.1, 2.4, 3.5, 8.9, 7.6),
+#'   display = c(1, 1, 1, 0, 0, 0)
+#' )
+#'
+#' test_data <-
+#' within(test_data, action <- factor(action, levels=c("Expanded", "Reduced", "No Change")))
+#'
+#' test_data <- test_data %>%
+#'  dplyr::arrange(action)
+
+
+#' create_tick_bar(
+#'   data = test_data,
+#'  x_variable = "action",
+#'   y_variable = "value",
+#'  x_group = "display",
+#'  tooltip = "Value: {point.value} <br> "
+#' )
+
+
+create_tick_bar <- function(
+    data,
+    type = "bar",
+    x_variable,
+    y_variable,
+    x_group,
+    size = 400,
+    y_format = "{value:,.0f}",
+    my_colors = c(naccho.viz::naccho_offwhite, naccho.viz::naccho_teal),
+    title_text = "",
+    subtitle_text = "",
+    caption_text = "",
+    caption_align = "center",
+    tooltip,
+    allow_export = TRUE,
+    accessible_desc = ""
+) {
+
+  hc <-
+    highcharter::hchart(
+      data,
+      type = type,
+      highcharter::hcaes(
+        x = .data[[x_variable]],
+        y = .data[[y_variable]],
+        group = .data[[x_group]]
+      ),
+      stacking = "normal"
+    ) %>%
+    highcharter::hc_add_dependency(
+      name = "modules/accessibility.js"
+    ) %>%
+    highcharter::hc_plotOptions(
+      accessibility = list(
+        enabled = TRUE,
+        description = accessible_desc,
+        keyboardNavigation = list(
+          enabled = TRUE
+        )
+      )
+    ) %>%
+    highcharter::hc_xAxis(
+      title = list(text = "") # x-axis label
+    ) %>%
+    highcharter::hc_yAxis(
+      title = list(text = ""), # y-axis label
+      max = 10,
+      tickAmount = 11,
+      min = 0,
+      labels = list(
+        format = y_format
+      )
+    ) %>%
+    highcharter::hc_colors(
+      colors = my_colors
+    ) %>%
+    highcharter::hc_title(
+      text = title_text, # title text
+      margin = 40,
+      widthAdjust = -60
+    ) %>%
+    highcharter::hc_size(
+      width = size
+    ) %>%
+    highcharter::hc_subtitle(
+      text = subtitle_text # text under title
+    ) %>%
+    highcharter::hc_caption(
+      text = caption_text, # source and data notes
+      useHTML = TRUE,
+      margin = 20,
+      align = caption_align
+    ) %>%
+    highcharter::hc_legend(
+      enabled = FALSE
+    ) %>%
+    highcharter::hc_tooltip(
+      pointFormat = tooltip,
+      useHTML = TRUE
+    ) %>%
+    highcharter::hc_add_theme(
+      naccho.viz::naccho_minimalTick_theme
+    ) %>%
+    highcharter::hc_exporting(
+      enabled = allow_export,
+      accessibility = list(
+        enabled = TRUE
+      ),
+      formAttributes = list(
+        target = "_blank" # opens in new window
+      ),
+      sourceHeight = 700, # sets height of download
+      sourceWidth = 1200, # sets width of download
+      allowHTML = TRUE,
+      url = "https://exporter.opifex.org", #serve owned by John Coene
+      buttons = list(
+        contextButton = list(
+          symbol = "menu",
+          symbolStrokeWidth = 2,
+          symbolFill = naccho.viz::naccho_teal,
+          symbolStroke = naccho.viz::naccho_teal,
+          menuItems = c("printChart",
+                        "separator",
+                        "downloadPNG",
+                        "downloadJPEG",
+                        "downloadPDF",
+                        "downloadSVG",
+                        "separator",
+                        "downloadCSV",
+                        "downloadXLS",
+                        "openInCloud")
+        )
+      )
+    )
+  return(hc)
+}
+
--- R/theme.R
+++ R/theme.R
@@ -7,3 +7,14 @@
 #'
 #' @source adapted from the NACCHO style guide for greater accessibility
 "naccho_minimal_theme"
+
+
+#' NACCHO Minimal Tick Theme
+#'
+#' A list containing NACCHO's minimal highcharter theme for tick bars
+#'
+#' @format ## `naccho_minimalTick_theme`
+#' A list with a length of 12
+#'
+#' @source adapted from the NACCHO style guide and made only for use with tick bars
+"naccho_minimalTick_theme"
